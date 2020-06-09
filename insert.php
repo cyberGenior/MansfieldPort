aba91335f96eb77006120ddf79a36f325b26659f
cyberGenior <63002375+cyberGenior@users.noreply.github.com>
2020-06-09 02:17:04 -0700

    connection fixingn

--- insert.php
+++ insert.php
@@ -1,44 +1,62 @@
 <?php
 
-			include_once 'connect.php';
+if(isset($_POST['submit1'])){
+
+	$con=mysqli_connect('db4free.net:3306','xpress', 'freedom98','xegger'); 
+
+	$First_name = $_POST['First_name'];
+
+	$Surname = $_POST['Surname'];
+
+	$DOB = $_POST['DOB'];
+
+	$SID = $_POST['SID'];
+
+	$Email = $_POST['Email'];
+
+	$Program = $_POST['Program'];
+
+	$MOS = $_POST['MOS'];
+
+	$Password1 = $_POST['Password1'];
+
+	$Password_verify = $_POST['Password_verify'];
 
 
-			$Data = $_POST['course'];
+	$stmt = $con->prepare("INSERT INTO `students` (First_name, Surname, DOB, SID, Email, Program, MOS) VALUES (?, ?, ?, ?, ?, ?, ?)");
 
-			
+	$stmt ->bind_param("sssisss",$First_name, $Surname, $DOB, $SID, $Email, $Program, $MOS);
 
-			$box = $_POST['box'];
-
-			
-
-			
-			if(isset($_POST['box'] = 'monday') && isset($_POST['submit'])){
-
-				$sql_insert = ("INSERT INTO `` (Day,Period,Course) VALUES(?,?,?)");
-
-			$stmt = $con->prepare($sql_insert);
-
-			$stmt->bind_param("sss", $day,$period,$course);
-
-			$stmt->execute();
-
-			$stmt->close();
-
-
-
-			}
-			
-			
-			
-		
+	$stmt->execute();
 
 
 
-?>
 
-<?php
 
-$error = error();
+	$stmt->close();
+
+	if($stmt->execute() == !error()){
+
+		echo "Connected successfully";
+
+	}
+
+
+
+
+	$con->close();
+
+
+}
+elseif (mysqli_connect_errno()) {
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+
+
+
+
+  header("Location: index.html");
 
 
 ?>
